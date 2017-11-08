099e8ab86801ee2a9af106c7f5279a748d40c012
felipexp90 <felipexp90@gmail.com>
2017-11-07 23:05:21 -0500

    cambios de michael de scrolling

--- js/Controler-Angular.js
+++ js/Controler-Angular.js
@@ -1,4 +1,4 @@
-var app = angular.module("myapp",['ui.bootstrap']);
+var app = angular.module("myapp",['ui.bootstrap','infinite-scroll']);
 
  app.controller("headercontroller", function($scope){  
       $scope.countryList = [  
@@ -54,6 +54,71 @@ var app = angular.module("myapp",['ui.bootstrap']);
       });
  });
  
-  
+ app.controller('ProductoController', function($scope, Reddit, $http) {
+	$scope.reddit = new Reddit();
+	$scope.productImageSmallBaseUrl = 'http://laptop-diego:9091/api/ImageSmall/';
+	loadCategorias();
+	$scope.onChange = onChange;
+	function loadCategorias() {
+		$http.get('http://laptop-diego:9091/api/categorias?page=0&size=1000s').success(function(data) {
+		  $scope.categorias = data;
+		}.bind(this));
+	}
+
+	function onChange() {
+		$scope.reddit.items = [];
+		$scope.reddit.page = 0;
+		$scope.reddit.total_pages = 0;
+		$scope.reddit.nextPage();
+	}
+});
+
+// Reddit constructor function to encapsulate HTTP and pagination logic
+app.factory('Reddit', function($http) {
+  var Reddit = function() {
+    this.items = [];
+    this.busy = false;
+    this.after = '';
+	this.page = 0;
+	this.total_pages = 0;
+	this.codigoProducto = null;
+	this.nombreProducto = null;
+	this.descripcion = null;
+	this.categoriaSeleccionada = 'Celulares';
+  };
+
+  Reddit.prototype.nextPage = function() {
+    if (this.busy) return;
+    this.busy = true;
+
+	var url = "http://laptop-michael:7076/api/producto/buscar/scroll?page=" + this.page + '&items_per_page=20';
+	if(this.codigoProducto !== null){
+		url += '&id=' + this.codigoProducto ;
+	}
+	if(this.nombreProducto !== null){
+		url += '&nombre=' + this.nombreProducto ;
+	}
+	if(this.descripcion !== null){
+		url += '&descripcion=' + this.descripcion ;
+	}
+	if(this.categoriaSeleccionada !== null){
+		url += '&categoria=' + this.categoriaSeleccionada ;
+	}
+	
+	
+    $http.get(url).success(function(data) {
+      var items = data.productos;
+	  this.page = data.page.number + 1;
+	  this.total_pages = data.page.total_pages;
+      for (var i = 0; i < items.length; i++) {
+        this.items.push(items[i]);
+      };
+      this.after = "t3_" + this.items[this.items.length - 1].id;
+      this.busy = false;
+    }.bind(this));
+  };
+
+  return Reddit;
+});
  
  
